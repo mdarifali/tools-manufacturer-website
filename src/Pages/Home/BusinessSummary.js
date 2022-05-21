8a1ac2c8c02805af560a98806a8ea3634677d312
mdarifali <arif.raj2050@gmail.com>
2022-05-22 02:29:47 +0600

    firebase auth added

---
+++ src/Pages/Home/BusinessSummary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import client from '../../assets/images/client.jpg';
+
+const BusinessSummary = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BusinessSummary;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../Shared/Footer';
+import BusinessSummary from './BusinessSummary';
 import Carousel from './Carousel';
 
 const Home = () => {
     return (
         <div>
            <Carousel />
+           <BusinessSummary />
            <Footer />
         </div>
     );
